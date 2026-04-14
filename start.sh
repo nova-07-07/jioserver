fb38629384f691f3f396f2b7be30b1a200259e77
nova-07-07 <snova234607ns@gmail.com>
2026-04-14 18:10:40 +0530

    v3

--- start.sh
+++ start.sh
@@ -2,13 +2,11 @@
 
 echo "Starting JioTV Go..."
 
-# If Render gives PORT use it, else 5001
 PORT=${PORT:-5001}
 
-# Start server in loop (prevent exit)
 while true
 do
-  ./jiotv_go serve --port $PORT --skip-update-check
+  ./jiotv_go --skip-update-check serve --port $PORT --public
   echo "Crashed. Restarting in 5 sec..."
   sleep 5
 done
